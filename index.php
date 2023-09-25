1ef97efc560ada63a2c1d7c9df9d62f1881231d4
ClauMndz <claudiamendezt19@gmail.com>
2023-09-25 16:53:51 -0500

    etiquetas h

--- index.php
+++ index.php
@@ -5,19 +5,19 @@ include "includes/head.php";?>
     <div class="column flex w_100">
         <aside class="column w_25 filters graybg m_outter">
             <button class="fw500" id="resetfilters">Limpiar filtros</button>
-            <h3 class="fw900">Encuentra hoteles en Bogotá</h3>
+            <h1 class="fw900">Encuentra hoteles en Bogotá</h1>
             <div class="filtergroup checkboxes color" data-filterid="tipos_de_hotel">
                 <h4 class="fw700"><span class="arrow"></span>Tipo de hotel</h4>
                 <div class="loader"></div>
                 <div class="content"></div>
             </div>
             <div class="filtergroup checkboxes color" data-filterid="servicios_de_hoteles">
-                <h4 class="fw700"><span class="arrow"></span>Servicios del hotel</h4>
+                <h2 class="fw700"><span class="arrow"></span>Servicios del hotel</h2>
                 <div class="loader"></div>
                 <div class="content"></div>
             </div>
             <div class="filtergroup checkboxes color" data-filterid="rangos_de_precios_hoteles">
-                <h4 class="fw700"><span class="arrow"></span>Rango de precios</h4>
+                <h3 class="fw700"><span class="arrow"></span>Rango de precios</h3>
                 <div class="loader"></div>
                 <div class="content"></div>
             </div>
